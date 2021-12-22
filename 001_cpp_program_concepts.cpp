cbe7887263e65b5767bf51effd888977e5dea81d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-22 23:19:02 +0330

    Implementation of -a complete c++ program-

--- 001_cpp_program_concepts.cpp
+++ 001_cpp_program_concepts.cpp
@@ -1,4 +1,12 @@
 //
 // Created by elns on 12/22/21.
 //
+// A complete c++ program
 
+#import <iostream>
+
+int main(){
+    int answer {1996};
+    std::cout <<" The answer is " << answer << std::endl;
+    return 0;
+}
