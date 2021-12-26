68b0b834054d336a26aa778039d5f82ad05e0c49
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-26 21:42:09 +0330

    Adding names and keywords convention

--- 001_cpp_program_concepts.cpp
+++ 001_cpp_program_concepts.cpp
@@ -11,8 +11,20 @@ int main(){
     int answer {1996};
     std::cout <<" The answer is " << answer << std::endl;
     /*************\
-     Another style
-     of comments
+
+     Names and keywords convention:
+     1. A name can be any sequence of upper- or lowercase letters A to Z or a to z, the digits 0
+        to 9, and the underscore character, _.
+     2. A name must begin with either a letter or an underscore.
+     3. Names are case sensitive.
+
+     Other names that you are not supposed to use include the following:
+     1. Names that begin with two consecutive underscores
+     2. Names that begin with an underscore followed by an uppercase letter
+     3. Within the global namespace: all names that begin with an underscore
+
+     Advice:
+     Do not use names that start with an underscore.
     \*************/
     return 0;
 }
